0b7f9108e329ad656ddaa8ffc08c9c639c748c49
andyzhou <302601530@qq.com>
2018-10-23 14:44:45 +0800

    Initial Commit

---
+++ U18Learn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  U18Learn
+//
+//  Created by 周冠羽 on 2018/10/23.
+//  Copyright © 2018 周冠羽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
